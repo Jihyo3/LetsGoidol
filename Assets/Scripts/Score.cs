1424a961b5f368e706ae62e7e0d34cc13281a4eb
YuunGu <116863451+YuunGu@users.noreply.github.com>
2024-05-17 20:08:34 +0900

    [Fix] Scroe

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    private Text score;
+    private TMP_Text score;
     private float time = 0f;
 
     private void Awake()
     {
-        score = GetComponent<Text>();
+        score = GetComponent<TMP_Text>();
     }
 
     private void Update()
