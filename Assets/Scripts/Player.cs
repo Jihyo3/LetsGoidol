3b0bcaa33060496797b56aeb227eeace1f0f17f6
jihyo <jgh6213@naver.com>
2024-05-20 11:09:55 +0900

    player right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(1f, 0, 0);
+        transform.position += Vector3.right * 0.05f;
     }
 }
