f259c4b227dddf82d5342b4381b32b003e82335c
MilkyQuartz <milkyquartz.2001@gmail.com>
2024-05-20 11:25:06 +0900

    GameOver UI
    + 임시구현
    + 초기화면으로 돌아갈 시 로그인 투명도 조절 재시작하게 수정

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    public void RetryBtnClick()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void StartSceneBtnClick()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
