316ae7d567893bd95bd88fe4a03ff8d978ca5d32
jihyo <jgh6213@naver.com>
2024-05-20 19:55:32 +0900

    Player init

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Player : MonoBehaviour
 {
+    float direction = 0.05f;
+    float speed = 6;
+    SpriteRenderer renderer;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.right * 0.05f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-speed * Time.deltaTime,0,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(speed * Time.deltaTime,0,0);
+        }
+        if (transform.position.x > 8.4f)
+        {
+            renderer.flipX = true;
+            direction = -0.05f;
+        }
+        if (transform.position.x < -8.4f)
+        {
+            renderer.flipX = false;
+            direction = 0.05f;
+        }
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (pos.x < 0f) pos.x = 0f;
+        if (pos.x > 1f) pos.x = 1f;
+        if (pos.y < 0f) pos.x = 0f;
+        if (pos.y > 1f) pos.x = 1f;
+
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
