df417846ba6a91e699b8060b73d700db8854ca89
ParkInsu456 <pisis7979@naver.com>
2024-05-22 11:40:02 +0900

    Feat: Missile base
    테스트씬에서 미사일 기본기능 구현. 보수필요

--- Assets/Scripts/Disruptor/Intercept/Intercept_Missile.cs
+++ Assets/Scripts/Disruptor/Intercept/Intercept_Missile.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Intercept_Missile : Missile
 {
@@ -13,8 +14,10 @@ public class Intercept_Missile : Missile
 
     private LayerMask targetLayer;  //LayerMask는 이진수비트다.
  
-    [SerializeField] private float range = 500f;
-    [SerializeField] private float speed = 20f;
+    [SerializeField] private float range = 20f;
+    [SerializeField] private float speed = 30f;
+
+    private bool IsLaunched = false;
 
 
     private void Awake()
@@ -29,31 +32,61 @@ public class Intercept_Missile : Missile
         targetLayer = LayerMask.NameToLayer("FallingObject");
     }
 
+
+    private void FixedUpdate()
+    {
+        if (IsLaunched && direction!=null)
+        Movemove();
+    }
+
+    // Launch 를 호출하면 픽스드 업데이트가 실행된다.
     public override void Launch()
     {
+        IsLaunched = true;
         GetTarget();
+    }
+
+    public void Movemove()
+    {
         rb.velocity = direction * speed;
     }
 
     protected override void GetTarget()
     {
         Vector2 playerPos = player.transform.position;
-        GameObject target = ladar.SearchClosestColliderInCircle(playerPos, range, targetLayer); //  1 <<6
+        GameObject target = ladar.SearchClosestColliderInCircle(playerPos, range, 1 << 6); //  
         direction = ladar.GetDirection(target.transform.position, playerPos);
     }
 
     void Hit(Collision2D collision)
     {
-        // 충돌하면 미사일에 맞는 효과를 발생시킴. 이는 자식클래스에서 효과를 구현.
+        // 충돌하면 미사일에 맞는 효과를 발생시킴. 
         collision.gameObject.SetActive(false);
+        IsLaunched = false;
+    }
+    void Hit(Collider2D collision)
+    {
+        collision.gameObject.SetActive(false);
+        IsLaunched = false;
+        Destroy(gameObject);
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    LayerMask collisionLayer = targetLayer;
+    //    if (collisionLayer.value == (collisionLayer.value | (1 << collision.gameObject.layer)))
+    //    {
+    //        Debug.Log("Hit");
+    //        Hit(collision);
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        LayerMask collisionLayer = targetLayer;
-        if (collisionLayer == (collisionLayer | (1 << collision.gameObject.layer)))
+        if (collision.CompareTag("FallingObject"))
         {
+            Debug.Log("Hit trigger");
             Hit(collision);
         }
     }
--- Assets/Scripts/Disruptor/Intercept/MissileStation.cs
+++ Assets/Scripts/Disruptor/Intercept/MissileStation.cs
@@ -8,25 +8,22 @@ public class MissileStation : MonoBehaviour
     // 얻은 미사일을 관리하는 클래스. 미사일 puch, 미사일pop
     // 5개짜리 리스트
     Stack<Missile> missileStation = new Stack<Missile>();
-    [SerializeField] private Missile obj;
+    [SerializeField] private Missile prefab;
     int maxMissile = 5;
 
-    private void Awake()
-    {
-        
-    }
-    private void Update()
+
+    private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.M))
         {
-            if (!missileStation.Contains(obj)) { return; }
+            if (!missileStation.Contains(prefab)) { return; }
             missileStation.Pop().Launch();
         }
     }
 
     public void PushMissile()
     {
-        missileStation.Push(obj);
+        missileStation.Push(prefab);
     }
 
     public void PopMissile()
@@ -36,10 +33,15 @@ public class MissileStation : MonoBehaviour
 
     public void DebugPushPushMissile()
     {
-        for (int i = 0; i < 5;  i++)
-        {
-            missileStation.Push(obj);
-        }
+        
+            MakeMissile();
+        
         missileStation.Pop().Launch();
+    }
+
+    // 이벤트가 작동하면 미사일을 생성하는 함수
+    public void MakeMissile()
+    {
+        missileStation.Push(Instantiate(prefab, transform));
     }
 }
